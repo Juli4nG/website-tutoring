aab1bbf3d0bd4e3a16f95f7aacdbc7041ee7d019
Julian Gonzalez <julian@avantelogic.com>
2026-09-03 12:23:00 -0700

    Added favorite only filter

--- scripts.js
+++ scripts.js
@@ -19,6 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const dropdown = document.querySelector("#genre-dropdown");
 
+  const favoritesOnly = document.querySelector("#favorites-only");
+  favoritesOnly.addEventListener("change", applyFilters);
+
   function renderGenreOptions(gamesList) {
     const uniqueGenres = new Array();
 
@@ -61,6 +64,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (query) {
       filteredGames = filteredGames.filter((game) =>
         game.title.toLowerCase().includes(query),
+      );
+    }
+
+    if (favoritesOnly.checked) {
+      filteredGames = filteredGames.filter((game) =>
+        favorites.includes(game.id),
       );
     }
 
